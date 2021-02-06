21c90e3cd7fcfb784efa4b5373fc24e055974848
张元 <zhangyuan@ctsi.com.cn>
2021-02-06 23:05:00 +0800

    添加element样式

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { registeGlobalComponent } from '@/components'
+import '@/styles/index.less'
 
 const app = createApp(App)
 
